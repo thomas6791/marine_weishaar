ace6d5a17e511510756dda0a519c55384290b827
thomas hunckler <hunckler.thomas@hotmail.fr>
2022-09-21 19:56:47 +0200

    change

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,6 +1,6 @@
 class ContactMailer < ApplicationMailer
     default from: 'contact@marine-weishaar.com'
     def new_contact_email
-        mail(to: "contact@marine-weishaar.com", subject: "You got a new contact!")
+        mail(to: "mw@marine-weishaar.com", subject: "You got a new contact!")
     end
 end
