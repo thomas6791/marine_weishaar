21bdbdcebcf003ea803e60b6b777e80174565c4d
thomas hunckler <hunckler.thomas@hotmail.fr>
2022-09-28 22:27:44 +0200

    add form to create + migration + last annonces

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    @last_annonces = Annonce.last(4)
   end
   def contact
   end
