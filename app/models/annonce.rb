8a32001c76d3188ce3dc5fcefe24e1a4ee178b8c
thomas hunckler <hunckler.thomas@hotmail.fr>
2022-10-20 19:39:09 +0200

    add geocoder

--- app/models/annonce.rb
+++ app/models/annonce.rb
@@ -1,4 +1,6 @@
 class Annonce < ApplicationRecord
     has_many_attached :photos
     monetize :price_cents
+    geocoded_by :address
+    after_validation :geocode, if: :will_save_change_to_address?
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_01_111625) do
+ActiveRecord::Schema.define(version: 2022_10_20_173424) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -68,6 +68,8 @@ ActiveRecord::Schema.define(version: 2022_10_01_111625) do
     t.string "dpe"
     t.string "ges"
     t.integer "price_cents", default: 0, null: false
+    t.float "latitude"
+    t.float "longitude"
   end
 
   create_table "users", force: :cascade do |t|
