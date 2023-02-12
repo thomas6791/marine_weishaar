4289e3a736b1ba113520cc659b0fa1bd3a77f6b6
thomas hunckler <hunckler.thomas@hotmail.fr>
2023-02-12 17:51:29 +0100

    add honeypot

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,10 +9,14 @@ class PagesController < ApplicationController
   end
   def contact_send
     contact = params[:contact_mail]
-    if contact[:rgpd] == "positive"
-      contact[:rgpd] = "oui"
+    if contact[:hidden_message].present?
+      redirect_to request.referrer
     else
-      contact[:rgpd] = "non"
+      if contact[:rgpd] == "positive"
+        contact[:rgpd] = "oui"
+      else
+        contact[:rgpd] = "non"
+      end
     end
   
     ContactMailer.with(status:contact[:status],type_demande:contact[:demande],email: contact[:email], lastname: contact[:lastname], firstname: contact[:firstname], tel: contact[:tel], message: contact[:message], rgpd: contact[:rgpd]).new_contact_email.deliver_later
