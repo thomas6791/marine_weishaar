767884c4d6c0adfd89e311b45b9c4253d15f3e0b
thomas hunckler <hunckler.thomas@hotmail.fr>
2023-05-07 18:16:27 +0200

    change

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :annonces, path:'achat-immobilier'
-  
+  scope module: 'annonces', path: 'achat-immobilier' do
+    resources :annonces
+  end
   root to: "pages#home"
   get "contact", to: "pages#contact"
   post "contact_send", to: "pages#contact_send"
+
+  get "achat-immobiler/strasbourg", to: "annonces#strasbourg"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
